efca63af7b0d309d8b719626aa3c96f0f1c17c6f
Sipho SK <sipssman@gmail.com>
2021-11-04 01:59:13 +0200

    Intiial full project commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,195 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { Table, Button, Pagination } from "react-bootstrap";
+import ProductModal from "././components/Modal/ProductModal";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import swal from "sweetalert";
 
 function App() {
+  const [editProduct, setEditProduct] = useState();
+
+  //loading, error and product states
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  //Modal states
+  const [modalShow, setModalShow] = useState(false);
+  const [addProduct, setAddProduct] = useState(true);
+
+  //Table Pagination states
+  const [page, setPage] = useState(1);
+  const [ascending, setAscending] = useState(false);
+  const [pageNumbers, setPageNumbers] = useState(0);
+
+  const pageSize = 50;
+  const APIUrl =
+    "https://gendacproficiencytest.azurewebsites.net/API/ProductsAPI";
+
+  // Fetching the data we wish to use, put it in descending order so new products can be seen
+  useEffect(() => {
+      fetch(
+        APIUrl +
+          "?page=" +
+          page +
+          "&pageSize=" +
+          pageSize +
+          "&ascending=" +
+          ascending
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setProducts(result.Results);
+            setPageNumbers(result.TotalNumberOfPages);
+          },
+
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+  }, [page]);//Use Effect will rerun the query every time there is a change to the page numbers
+
+
+  //Function used to send a delete request to the API, to delete the specific item
+  function deleteProduct(name, id) {
+    //Here we delete the product we clicked on
+    axios.delete(`${APIUrl}/${id}`).then((res) => {
+      //After we receive the data we show an alert to let them know it has been deleted
+      swal({
+        title: "Product Deleted!",
+        text:
+          "Product " +
+          `${id}` +
+          "\n Name: " +
+          `${name}` +
+          "\n Was successfully deleted!",
+        icon: "success",
+        buttons: "Great!",
+      })
+        //After they close the modal we then refresh the page to get our data
+        .then((value) => {
+          window.location.reload();
+        });
+    });
+  }
+
+  //We edit our variables to get the add product variable
+  function showAddProduct() {
+    setAddProduct(true);
+    setModalShow(true);
+  }
+
+  //We edit our state variables to get the correct modal to open
+  function showEditProduct(id) {
+    setEditProduct(id);
+    setAddProduct(false);
+    setModalShow(true);
+  }
+
+  function goNextPage() {
+    //Check if we are at the end of our list if we are we loop to the front
+    if(page == pageNumbers){
+      setPage(1)
+      return
+    }
+    setPage(page + 1);
+  }
+
+  function goPreviousPage() {
+    //Check if we are at the start of our list if we are we go to the end
+    if(page == 1){
+      setPage(pageNumbers)
+      return
+    }
+    setPage(page - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="Product_bar">
+        Product List{" "}
+        <Button variant="success" onClick={() => showAddProduct()}>
+          Add Product
+        </Button>
+      </h2>
+      <br />
+      <Table
+        striped
+        borded
+        hover
+        size="sm"
+        style={{
+          marginTop: 30,
+        }}
+      >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!isLoaded
+            ? "Loading..."
+            : products.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.Id}</td>
+                  <td>{item.Name}</td>
+                  <td>{item.Price}</td>
+                  <td>{item.Category}</td>
+                  <td>
+                    <Button
+                      variant="primary"
+                      onClick={() => showEditProduct(item.Id)}
+                    >
+                      Edit
+                    </Button>
+                  </td>
+                  <td>
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        deleteProduct(item.Name, item.Id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+        </tbody>
+      </Table>
+      <div style={{ marginBottom: 30 }}>
+        <Button variant="outline-primary" onClick={() => goPreviousPage()}>
+          Previous Page
+        </Button>
+        <Button variant="outline-primary" onClick={() => goNextPage()}>
+          Next Page
+        </Button>
+        <div>
+          Page {page} of {pageNumbers}
+        </div>
+      </div>
+
+      {/* Below is the modal, which shows when the button is clicked a different modal is shown depending on which button is clicked */}
+      <ProductModal
+        addproduct={addProduct}
+        productid={editProduct}
+        show={modalShow}
+        url={APIUrl}
+        onHide={() => setModalShow(false)}
+      />
+      <ToastContainer position="top-right" autoClose={1500} />
     </div>
   );
 }
---
+++ src/components/Modal/ProductModal.js
@@ -0,0 +1,144 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Modal, Button, Form, Row, Col } from "react-bootstrap";
+import { toast } from "react-toastify";
+
+const ProductModal = (props) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState(0);
+  const [editing, setEditing] = useState(props.newEdit);
+
+  //If we wish to edit a product then we request its details only once to fill in our fields
+  useEffect(() => {
+    fetch(props.url + "/" + props.productid)
+      .then((res) => res.json())
+      .then((result) => {
+        setName(result.Name);
+        setPrice(result.Price);
+        setCategory(result.Category);
+      });
+  }, [props.productid]);//We check to see if the id has changed, if it has we update our modal
+
+  //Function to make form submission
+  const submitform = async (e) => {
+    e.preventDefault();
+
+    //We check if this is a new product or existing product to see if we update or post
+    if (props.productid) {
+      const Id = props.productid;
+      const updatedProduct = {
+        Id: Id,
+        Name: name,
+        Category: category,
+        Price: price,
+      };
+      const res = await axios({
+        method: "put",
+        url: props.url + "/" + props.productid,
+        data: JSON.stringify(updatedProduct),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then(() => {
+          toast.success("Product Updated!", {
+            onClose: () => window.location.reload(),
+            theme: "dark",
+          });
+          toast.clearWaitingQueue(); //Removes any extra toasts queued up
+        })
+        .catch((error) => {
+          toast.error(error.response.data.Message);
+        });
+    } else {
+      //If it is a new product we then add it via post request
+      const product = { name, price, category };
+      //make our post request
+      const res = await axios({
+        method: "post",
+        url: props.url,
+        data: JSON.stringify(product),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then(() => {
+          toast.success("Product added!", {
+            onClose: () => window.location.reload(),
+          });
+        })
+        .catch((error) => {
+          if (error.response) {
+            toast.error(error.response.data.Message);
+          }
+        });
+    }
+  };
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.addproduct ? "Add a new product" : "Edit Product Details"}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={submitform}>
+          <Row className="mb-3">
+            <Form.Group as={Col}>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                className="name"
+                type="text"
+                placeholder="Product name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                className="formPrice"
+                type="number"
+                placeholder="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </Form.Group>
+          </Row>
+
+          <Form.Label>Category</Form.Label>
+          <Form.Select
+            aria-label="Default select example"
+            className="formCategory"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option>Please select a category</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </Form.Select>
+          <Button
+            style={{
+              marginTop: 10,
+            }}
+            variant="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="danger" onClick={props.onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ProductModal;
