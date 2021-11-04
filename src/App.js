852711ea2f5095bc623bf3412b2a54ef13a62291
Sipho SK <sipssman@gmail.com>
2021-11-04 02:51:59 +0200

    Index css seems to have been the issue

--- src/App.js
+++ src/App.js
@@ -177,26 +177,6 @@ function App() {
         <div>
           Page {page} of {pageNumbers}
         </div>
-        <ButtonToolbar>
-          <Button variant="primary">Primary</Button>
-          <Button variant="secondary">Secondary</Button>
-          <Button variant="success">Success</Button>
-          <Button variant="warning">Warning</Button>
-          <Button variant="danger">Danger</Button>
-          <Button variant="info">Info</Button>
-          <Button variant="light">Light</Button>
-          <Button variant="dark">Dark</Button>
-          <Button variant="link">Link</Button>
-          /* Outline version of the main variants */
-          <Button variant="outline-primary">Primary</Button>
-          <Button variant="outline-secondary">Secondary</Button>
-          <Button variant="outline-success">Success</Button>
-          <Button variant="outline-warning">Warning</Button>
-          <Button variant="outline-danger">Danger</Button>
-          <Button variant="outline-info">Info</Button>
-          <Button variant="outline-light">Light</Button>
-          <Button variant="outline-dark">Dark</Button>
-        </ButtonToolbar>
       </div>
 
       {/* Below is the modal, which shows when the button is clicked a different modal is shown depending on which button is clicked */}
